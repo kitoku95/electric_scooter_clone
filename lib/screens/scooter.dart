9762065a6d76b9b94335bafc7ff6dd4dd65a264f
kitoku95 <caocongduc95@gmail.com>
2021-08-09 16:57:13 +0900

    update UI

--- lib/screens/scooter.dart
+++ lib/screens/scooter.dart
@@ -1,32 +1,85 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:electric_scooter_clone/widgets/scooter_widget.dart';
 import 'package:flutter/material.dart';
 
 class ScooterScreen extends StatefulWidget {
-  const ScooterScreen({ Key key }) : super(key: key);
+  const ScooterScreen({Key key}) : super(key: key);
+
 
   @override
   _ScooterScreenState createState() => _ScooterScreenState();
 }
 
-class _ScooterScreenState extends State<ScooterScreen> {
+class _ScooterScreenState extends State<ScooterScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = new AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  double get maxHeight => MediaQuery.of(context).size.height;
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    _animationController.value -= details.primaryDelta / maxHeight;
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    if (_animationController.isAnimating ||
+        _animationController.status == AnimationStatus.completed) return;
+
+    final double flingVelocity =
+        details.velocity.pixelsPerSecond.dy / maxHeight;
+    if (flingVelocity < 0.0) {
+      _animationController.fling(velocity: max(2.0, -flingVelocity));
+    } else if (flingVelocity > 0.0) {
+      _animationController.fling(velocity: min(-2.0, -flingVelocity));
+    } else {
+      _animationController.fling(
+          velocity: _animationController.value < 0.5 ? -2.0 : 2.0);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        color: Theme.of(context).backgroundColor,
-        boxShadow: const<BoxShadow>[
-          BoxShadow(
-            color: Colors.black12,
-            offset: Offset(0, 20),
-            blurRadius: 12,
-          )
-        ]
-      ),
-      child: Stack(
-        children: [
-          
-        ],
-      ),
-    );
+    return AnimatedBuilder(
+        animation: _animationController,
+        builder: (BuildContext context, Widget child) {
+          return Transform.scale(
+            scale: lerpDouble(1.0, .8, _animationController.value),
+            alignment: Alignment.topRight,
+            child: GestureDetector(
+              onVerticalDragUpdate: _onDragUpdate,
+              onVerticalDragEnd: _onDragEnd,
+              child: Container(
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).backgroundColor,
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10 * 3 * _animationController.value)),
+                    boxShadow: const <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0, 20),
+                        blurRadius: 12,
+                      )
+                    ]),
+                child: Stack(
+                  children: [ScooterWidget(scooterDragPercent: _animationController.value,)],
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
--- lib/widgets/scooter_widget.dart
+++ lib/widgets/scooter_widget.dart
@@ -1,21 +1,153 @@
 import 'package:flutter/material.dart';
 
+import 'gradient_text.dart';
+
 class ScooterWidget extends StatefulWidget {
-  const ScooterWidget({ Key key }) : super(key: key);
+  const ScooterWidget({Key key, this.scooterDragPercent}) : super(key: key);
+
+  final double scooterDragPercent;
 
   @override
   _ScooterWidgetState createState() => _ScooterWidgetState();
 }
 
-class _ScooterWidgetState extends State<ScooterWidget> {
+class _ScooterWidgetState extends State<ScooterWidget>
+    with SingleTickerProviderStateMixin {
+  AnimationController _slideInAnimattionController;
+  Animation<double> _slideInAnimation;
 
-  Widget _buildCenterText(){
-    return null;
+  Offset _scooterOffset = const Offset(0, 0);
+  Offset _centerTextOffset = const Offset(0, 0);
+
+  double _bottomTextPosY = 120;
+
+  @override
+  void initState() {
+    _slideInAnimattionController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1200))
+      ..addListener(() {
+        setState(() {
+          _scooterOffset = Offset(-1 + _slideInAnimation.value, 0);
+          _centerTextOffset = Offset(1 - _slideInAnimation.value, 0);
+          _bottomTextPosY = 70 + (50 * _slideInAnimation.value);
+        });
+      })
+      ..forward();
+    _slideInAnimation = CurvedAnimation(
+        parent: _slideInAnimattionController, curve: Curves.ease);
+    super.initState();
   }
+
+  @override
+  void dispose() {
+    _slideInAnimattionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(ScooterWidget oldWidget) {
+    final double dragPercent = widget.scooterDragPercent;
+    if (oldWidget.scooterDragPercent != dragPercent) {
+      _scooterOffset = Offset(-.3 * dragPercent, -.3 * dragPercent);
+      _bottomTextPosY = 120 + (-200 * dragPercent);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  Widget _buildCenterText() {
+    return FractionalTranslation(
+        translation: _centerTextOffset,
+        child: Transform.scale(
+          scale: 1.0 - widget.scooterDragPercent.clamp(0.0, 0.1),
+          child: Center(
+            child: GradientText('Go',
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.black,
+                    Colors.blueAccent,
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                )),
+          ),
+        ));
+  }
+
+  Widget _buildScooterImage(double height) {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: height * .26,
+      child: FractionalTranslation(
+        translation: _scooterOffset,
+        child: Transform.scale(
+            scale: 1 + (widget.scooterDragPercent / 2),
+            child: Image.asset(
+              'assets/images/scooter.png',
+            )),
+      ),
+    );
+  }
+
+  Widget _buildBottomText() {
+    return Positioned(
+      bottom: _bottomTextPosY,
+      child: Container(
+        child: Text(
+          'go to where',
+          style: new TextStyle(
+            fontSize: 30,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        width: MediaQuery.of(context).size.width,
+      ),
+    );
+  }
+
+  Widget _buildBottomCard() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        width: double.infinity,
+        height: 90,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        child: Row(
+          children: [
+            Container(
+              width: 100,
+              height: 90,
+              decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                      colors: [Colors.blueAccent, Colors.lightBlue],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter),
+              borderRadius: BorderRadius.circular(20)),
+              child: Center(
+                child: ImageIcon(
+                  const AssetImage('assets/images/power.png'),
+                  size: 32,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final double height = MediaQuery.of(context).size.height;
+    return Stack(
+      children: [
+        _buildCenterText(),
+        _buildScooterImage(height),
+        _buildBottomText(),
+        _buildBottomCard()
+      ],
     );
   }
 }
